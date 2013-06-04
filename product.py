5fb3c872b322c4f5372d437324900acb29297be9
Iskandar Goh <incredible2288@gmail.com>
2013-06-05 00:04:13 +0100

    add some parsing image function

--- product.py
+++ product.py
@@ -1,7 +1,7 @@
 __author__ = 'Emmanouil Samatas'
 
 import pymongo
-from gevent import monkey; monkey.patch_all()
+#from gevent import monkey; monkey.patch_all()
 from bottle import *
 from bson.json_util import dumps, loads
 
@@ -75,5 +75,79 @@ def addProduct():
     #     response.status = 300
     #     return {'error': 'Insert Error : ' + str(e)}
 
+@post('/user/addFollower')
+def addFollower():
+    relationship = {}
+    session = {'username':"samatase"}
+    requestBody = loads(request.body.read())
+    token = requestBody['token']
+    relationship['following'] = requestBody['username']
+    relationship['user'] = session['username']
+
+    try:
+        connection = pymongo.MongoClient(connectionString)
+        db = connection.brazaar2
+
+        relationships = db.relationships
+
+        validate = list(db.users.find({"username":relationship['following']}))
+
+
+        if validate:
+            relationships.insert(dict(relationship))
+            return relationship
+        else:
+            response.status = 300
+            return 'Invalid username'
+
+    except pymongo.errors.PyMongoError as e:
+        response.status = 300
+        return {'error': 'Insert Error : ' + str(e)}
+
+@post ('/user/createUser')
+def createUser():
+    user = {}
+    requestBody = loads(request.body.read())
+    token = requestBody['token']
+    user['username'] = requestBody['username']
+    user['firstName'] = requestBody['firstName']
+    user['lastName'] = requestBody['lastName']
+
+
+    try:
+        connection = pymongo.MongoClient(connectionString)
+        db = connection.brazaar2
+        users = db.users
+        users.insert(user)
+    except pymongo.errors.PyMongoError as e:
+        response.status = 300
+        return {'error': 'Insert Error : ' + str(e)}
+
+@get ('/user/getFollowings')
+def getFollowing():
+    session = {'username':"samatase"}
+    followingUsers = []
+    requestBody = loads(request.body.read())
+    user = session['username']
+
+    try:
+        connection = pymongo.MongoClient(connectionString)
+        db = connection.brazaar2
+        followings = list(db.relationships.find({"user":user},{"following":1,"_id":0}))
+        for following in followings:
+            followingUsers.append(list(db.users.find({"username":following['following']},{"firstName":1,"lastName":1,"_id":0}).limit(1)))
+        return dumps(followingUsers)
+    except pymongo.errors.PyMongoError as e:
+        response.status = 300
+        return {'error': 'Connection Error : ' + str(e)}
+
+
+
+
+
+
+
+
+
 # run(host='ec2-54-228-18-198.eu-west-1.compute.amazonaws.com', port=8000, debug=True, reloader=True, server="gevent")
-run(host='127.0.0.1', port=8082, debug=True, reloader=True, server="gevent")
+run(host='127.0.0.1', port=8082, debug=True, reloader=True)
